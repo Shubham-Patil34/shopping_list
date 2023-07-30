ad9fc40b646980101c1b36990bb876a2588c42b6
Shubham Patil <shubham.patil@netcracker.com>
2023-07-30 14:54:56 +0530

    updated script for remove list item, clear all, and filter list items

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+// It wil not alter when a item is added or removed => define in function scope
+// const items = itemList.querySelectorAll('li');
 
+//-******************************* Add list item *******************************-//
 // Trigger functionality
 /**
  *
@@ -26,7 +31,10 @@ function additem(event) {
   btn = createButton('remove-item btn-link text-red');
 
   li.appendChild(btn);
+
   itemList.appendChild(li);
+  itemInput.value = '';
+  checkUI();
 }
 
 /**
@@ -64,7 +72,100 @@ function createIcon(classes) {
   return i;
 }
 
+//-******************************* Remove list item *******************************-//
+
+// Remove list item using delegation: Target parent and then child so if any child is added dynamically will also be considered
+function removeItem(event) {
+  if (event.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      event.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function highLight(event) {
+  if (event.target.className === 'fa-solid fa-xmark') {
+    event.target.parentElement.parentElement.style.outlineStyle = 'solid';
+    event.target.parentElement.parentElement.style.outlineWidth = '1px';
+    event.target.parentElement.parentElement.style.outlineColor = 'red';
+  }
+}
+
+function undoHighLight(event) {
+  if (event.target.className === 'fa-solid fa-xmark') {
+    event.target.parentElement.parentElement.style.outlineStyle = 'none';
+  }
+}
+
+//-******************************* Remove all list items: Clear *******************************-//
+function clearAllItems(event) {
+  if (confirm('Are you sure?')) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+  }
+}
+
+clearBtn.addEventListener('click', clearAllItems);
+
+//-******************************* Check UI status *******************************-//
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    itemFilter.style.display = 'none';
+    clearBtn.style.display = 'none';
+  } else {
+    itemFilter.style.display = 'block';
+    clearBtn.style.display = 'block';
+  }
+}
+
+//-******************************* filter items *******************************-//
+function filterItems(event) {
+  const items = itemList.querySelectorAll('li');
+  const text = event.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    console.log(itemName.indexOf(text));
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+
+  // Alternate way
+  // const items = itemList.querySelectorAll('li');
+  // const input = event.target.value.toLowerCase();
+  // console.log(items);
+  // if (items.length !== 0 && event.target.value !== '') {
+  //   // console.log('value:', event.target.value);
+  //   for (let item of items) {
+  //     const itemName = item.firstChild.textContent.toLowerCase();
+  //     if (itemName.includes(input)) {
+  //       // console.log(item);
+  //       item.style.display = 'flex';
+  //     } else {
+  //       item.style.display = 'none';
+  //     }
+  //   }
+  // } else {
+  //   for (let item of items) {
+  //     item.style.display = 'flex';
+  //   }
+  // }
+}
+
 // Event listeners
 itemForm.addEventListener('submit', additem);
 itemInput.addEventListener('submit', additem);
 itemList.addEventListener('submit', additem);
+itemFilter.addEventListener('keyup', filterItems);
+itemList.addEventListener('click', removeItem);
+itemList.addEventListener('mouseover', highLight);
+itemList.addEventListener('mouseout', undoHighLight);
+
+checkUI();
